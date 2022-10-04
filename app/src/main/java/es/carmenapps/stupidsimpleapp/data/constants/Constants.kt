d45a5b292384255274db5ed63ddf33646332ca95
Carmen <carmen.morales.bonet@gmail.com>
2022-10-04 09:08:47 +0200

    fixing costants

--- app/src/main/java/es/carmenapps/stupidsimpleapp/data/constants/Constants.kt
+++ app/src/main/java/es/carmenapps/stupidsimpleapp/data/constants/Constants.kt
@@ -2,7 +2,4 @@ package es.carmenapps.stupidsimpleapp.data.constants
 
 object AppConstants {
   const val DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
-  const val HOUR_FORMAT = "HH:mm:ss"
-  const val FEMALE = "F"
-  const val NOTHING = "NOTHING"
 }
